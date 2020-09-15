9db0b7f913973d311380039cdccc55ad7f1c1566
gaston <gassanchez14@gmail.com>
2020-09-14 21:07:15 -0300

    Update Main.java

--- src/practice4/exercise4/Main.java
+++ src/practice4/exercise4/Main.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] args) {
 
         SoccerPlayer mesias = new SoccerPlayer("leo", "messi", "BB45", LocalDate.of(1990, 4, 4), "lateral izquierdo", SkillfullLeg.RIGHT, 1);
-        SoccerPlayer mesisas = new SoccerPlayer("leo", "messi", "BB4ds5", LocalDate.of(1990, 4, 4), "lateral derecho", SkillfullLeg.LEFT_RIGHT, 18);
+        SoccerPlayer messias = new SoccerPlayer("leo", "messi", "BB4ds5", LocalDate.of(1990, 4, 4), "lateral derecho", SkillfullLeg.LEFT_RIGHT, 18);
 
-        System.out.println( mesias.getDisponible());
+        System.out.println(mesias.getDisponible());
        ;
 
 
